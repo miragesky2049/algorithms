b1f9de55a8f3b616f010bcb956ed08680aa8d818
zhenghaiming <zhenghaiming@beisen.com>
2017-12-01 12:56:41 +0800

    堆排序

--- AlgorithmsDemo/Program.cs
+++ AlgorithmsDemo/Program.cs
@@ -8,7 +8,7 @@ namespace AlgorithmsDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Program p = new Program();
             var a = new IComparable[] { 3, 1, 15, 6,45, 13, 51, 9, 45, 0, 85, 67, 39, 68, 42, 26, 457 };
@@ -302,6 +302,84 @@ namespace AlgorithmsDemo
             }//现在a[lo..lt-1]<v=a[lt..gt]<a[gt+1..hi]成立
             sort(a, lo, lt - 1);
             sort(a, gt + 1, hi);
+        }
+    }
+
+    //2.4 优先队列
+    //public class Key : IComparable
+    //{
+    //    public int CompareTo(object obj)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    // <summary>
+    // 算法2.6 基于堆的优先队列
+    // </summary>
+    /// <typeparam name="Key"></typeparam>
+    public class MaxPQ<Key> where Key:IComparable
+    {
+        private Key[] pq;//基于堆的完全按二叉树
+        private int N = 0;//存储于pq[1..N]中，pq[0]没有使用
+        public MaxPQ(int maxN)
+        {
+            pq = new Key[maxN + 1];
+        }
+
+        public bool isEmpty()
+        {
+            return N == 0;
+        }
+
+        public int size()
+        {
+            return N;
+        }
+
+        public void insert(Key v)
+        {
+            pq[++N] = v;
+            swim(N);
+        }
+
+        public Key delMax()
+        {
+            Key max = pq[1];
+            exch(1, N--);
+            pq[N + 1] = default(Key);
+            sink(1);
+            return max;
+        }
+        private bool less(int i,int j)
+        {
+            return pq[i].CompareTo(pq[j]) < 0;
+        }
+
+        private void exch(int i, int j)
+        {
+            Key t = pq[i]; pq[i] = pq[j];pq[j] = t;
+        }
+
+        public void swim(int k)
+        {
+            while(k>1 && less(k / 2, k))
+            {
+                exch(k / 2, k);
+                k = k / 2;
+            }
+        }
+
+        public void sink(int k)
+        {
+            while (2 * k <= N)
+            {
+                int j = 2 * k;
+                if (j < N && less(j, j + 1)) j++;
+                if (!less(k, j)) break;
+                exch(k, j);
+                k = j;
+            }
         }
     }
 }
